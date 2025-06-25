2c3a4ddb66c7bde86b3e385d574f069473a5111d
Nestor Gariglio <nestorgariglio33@gmail.com>
2025-06-25 19:26:01 -0300

    some fixes

--- app/Controllers/ViewsController.php
+++ app/Controllers/ViewsController.php
@@ -5,33 +5,23 @@ namespace App\Controllers;
 class ViewsController extends BaseController
 {
   public function principal() {
-    echo view('/layouts/header');
     echo view('principal');
-    echo view('/layouts/footer');
   }
 
   public function about() {
-    echo view('/layouts/header');
     echo view('about');
-    echo view('/layouts/footer');
   }
 
   public function contact() {
-    echo view('/layouts/header');
     echo view('contact');
-    echo view('/layouts/footer');
   }
 
   public function terms() {
-    echo view('/layouts/header');
     echo view('terms');
-    echo view('/layouts/footer');
   }
 
   public function marketing() {
-    echo view('/layouts/header');
     echo view('marketing');
-    echo view('/layouts/footer');
   }
 
 }
